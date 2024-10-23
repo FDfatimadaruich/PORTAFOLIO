a92d26ab72aece9af28d70740b08609053ed503a
FDfatimadaruich <fatimadaruich.fd@gmail.com>
2024-10-23 14:45:58 -0300

    ultimo

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // script.js
+// script.js
 document.getElementById('downloadBtn').addEventListener('click', function() {
-    // Cambia 'ruta/a/tu/cv.pdf' a la ruta real de tu archivo PDF
     const link = document.createElement('a');
-    link.href = 'PORTAFOLIO/CVFATIMA.pdf'; // Ruta del archivo PDF
-    link.download = 'CVFATIMA.pdf'; // Nombre con el que se descargará el archivo
+    link.href = 'https://github.com/FDfatimadaruich/PORTAFOLIO/blob/main/CVFATIMA%20(1).pdf'; // URL del PDF
+    link.download = 'mi_cv.pdf'; // Nombre con el que se descargará el archivo
     document.body.appendChild(link); // Añadir el enlace al DOM
     link.click(); // Hacer clic en el enlace
     document.body.removeChild(link); // Eliminar el enlace del DOM
 });
+
+
